ebbb0b22f3750f6ee34ba76640e9f99fa016888e
anyatibrian <anyatibrian@gmail.com>
2018-12-02 10:17:03 +0300

    adds the function that get all the parcel orders[finishes #162364056]

--- UI/static/js/parcelOrders.js
+++ UI/static/js/parcelOrders.js
@@ -1,7 +1,7 @@
 let create_orders=document.getElementById('submit');
-
 //get the access token in our local storage
 let token = localStorage.getItem('access-token');
+
 create_orders.addEventListener('click', createParcelOrders);
 
 function createParcelOrders(e) {
@@ -12,6 +12,7 @@ function createParcelOrders(e) {
     let pickup = document.getElementById('pickup').value;
     let destination=document.getElementById('destination').value;
     let weight =parseInt(document.getElementById('weight').value);
+
 
     // setting the data to be posted
     let data ={
@@ -46,15 +47,52 @@ function createParcelOrders(e) {
 
 // fetch all the parcels of a specific user
 window.onload = function loadParcelOrders(){
+
     fetch("http://127.0.0.1:5000/api/v1/parcels",{
         method:'GET',
         headers:{
-            'Application':'application/json, text/plain, */*',
-            'Content-type':'application/json'
+            'Content-type':'application/json',
+            Authorization:`Bearer ${token}`
         },
-        Authorization:`Bearer ${token}`
     }).then((response)=> response.json())
         .then(function (data){
-            alert(data['message']);
+            if(data['parcel_orders']){
+                let output =`<table id="myTable">
+                <tr class="header">
+                    <th style="width:10%;">Serial No.</th>
+                    <th style="width:10%;">Receiver</th>
+                    <th style="width:30%;">Description</th>
+                    <th style="width:10%;">Pick up</th>
+                    <th style="width:10%;">Destination</th>
+                    <th style="width:15%;">Current location</th>
+                    <th style="width:10%;">Price</th>
+                    <th style="width:10%;">Status</th>
+                    <th style="width:15%;">Delivery </th>
+                </tr>`;
+                data['parcel_orders'].forEach(function (parcelorder){
+                    localStorage.setItem('parcel_id', parcelorder.parcel_id);
+                    output+=`<tr>
+                    <td>${parcelorder.serial_no}</td>
+                    <td>${parcelorder.receivers}</td>
+                    <td>${parcelorder.description}</td>
+                    <td>${parcelorder.pickup}</td>
+                    <td>${parcelorder.destination}</td>
+                    <td>${parcelorder.current_location}</td>
+                    <td>${parcelorder.delivery_price}</td>
+                    <td>${parcelorder.status}</td>
+                    <td><button  class="button-success" onclick="update_parcel_order()">update</button></td>
+                </tr>`;
+                });
+                document.getElementById('parcels_content').innerHTML=output;
+            }else{
+                document.getElementById('parcels_content').style.color="red";
+                document.getElementById('parcels_content').innerHTML=`<h2>${data['errors']}</h2>`;
+            }
         });
+}
+
+// update parcel destination
+
+function updateDesitnation(){
+    alert('have been clicked');
 }
