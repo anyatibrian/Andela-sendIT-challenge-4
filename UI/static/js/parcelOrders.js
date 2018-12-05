ad6e7097e15fe2ea97fdd384206e3df925428715
anyatibrian <anyatibrian@gmail.com>
2018-12-05 13:23:31 +0300

    refactors the loadparcel function to caters to display appropiate message [finishes #162377054]

--- UI/static/js/parcelOrders.js
+++ UI/static/js/parcelOrders.js
@@ -85,6 +85,7 @@ window.onload = function loadParcelOrders(){
                 });
                 document.getElementById('parcels_content').innerHTML=output;
             }else{
+                // display message to  the user incase they don't have any parcel orders
                 document.getElementById('parcels_content').style.color="red";
                 document.getElementById('parcels_content').innerHTML=`<h2>empty parcel orders list</h2>`;
             }
