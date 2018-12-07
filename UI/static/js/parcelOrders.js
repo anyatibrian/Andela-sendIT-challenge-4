e22c28a2e579e1567a61d4ae2912881e8cc8a6f7
anyatibrian <anyatibrian@gmail.com>
2018-12-07 11:12:40 +0300

    adds the suchfunctionality to the search box [finishes #162377054]

--- UI/static/js/parcelOrders.js
+++ UI/static/js/parcelOrders.js
@@ -142,3 +142,29 @@ function updateStatus(parcel_id){
             window.location.replace('../templates/parcelOrder.html');
         });
 }
+
+//function that enables us to search through the product list
+function SearchParcelOrders(){
+    let table = document.getElementById('myTable');
+    let input = document.getElementById('search').value;
+    let search = input.toUpperCase();
+    let tr = table.getElementsByTagName('tr');
+
+    // loop through the table and find the one which matches our search result
+    let i;
+
+    for(i=0; i< tr.length; i++){
+
+        let td=tr[i].getElementsByTagName("td")[0];
+        if (td){
+            let searchResult = td.textContent|| td.innerText;
+            if (searchResult.toUpperCase().indexOf(search) > -1){
+                tr[i].style.display = "";
+            }
+            else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+
+}
